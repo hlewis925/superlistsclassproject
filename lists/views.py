9a6e002d73093999739f3764717e437cb10f31a1
angels98 <hlewisutube@gmail.com>
2016-10-25 13:36:38 -0500

    rename all item input ids and names. still broken

--- lists/views.py
+++ lists/views.py
@@ -19,3 +19,7 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
+
+except ValidationError:
+    error = "You can't have an empty list item"
+    return render(request, 'home.html', {"error": error})
