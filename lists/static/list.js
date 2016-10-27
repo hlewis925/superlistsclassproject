777ebc0f13f96891a94707484e2942202e09edc4
angels98 <hlewisutube@gmail.com>
2016-10-26 19:35:09 -0500

    add jquery, qunit tests, list.js with keypress listeners

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,9 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
+var initialize = function () {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
+};
+
 };
